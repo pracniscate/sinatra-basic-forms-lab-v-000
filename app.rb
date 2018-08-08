754cdbc855b6cbc55d7907292718eda647ef0d6c
Lorena <lorena.giddens@outlook.com>
2018-08-08 15:04:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,9 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @name = params[:name]
-    @breed = params[:breed]
-    @age = params[:age]
+    @puppy = Puppy.new(@name, @breed, @age)
     erb :display_puppy
   end
 
